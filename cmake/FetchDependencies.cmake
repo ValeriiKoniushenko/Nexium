b5d10563a6ce03ef1b41bdf6972c832f8c0ea205
Valerii Koniushenko <Valerii.Koniushenko@gmail.com>
2025-06-29 20:16:15 +0200

    First success launch with 'square' on the screen

--- cmake/FetchDependencies.cmake
+++ cmake/FetchDependencies.cmake
@@ -15,31 +15,21 @@ FetchContent_MakeAvailable(Utils)
 
 
 
-FetchContent_Declare(Glad
-        GIT_REPOSITORY  git@github.com:Dav1dde/glad.git
-        GIT_TAG         v2.0.8
-        GIT_SHALLOW     TRUE          # Enables --depth 1
-        GIT_PROGRESS    TRUE
-)
-FetchContent_MakeAvailable(Glad)
-
-
-
 FetchContent_Declare(Glfw
         GIT_REPOSITORY  git@github.com:glfw/glfw.git
         GIT_TAG         3.4
         GIT_SHALLOW     TRUE          # Enables --depth 1
         GIT_PROGRESS    TRUE
 )
-FetchContent_MakeAvailable(Glad)
+FetchContent_MakeAvailable(Glfw)
 
 
 
-FetchContent_Declare(QtBase
-        GIT_REPOSITORY  git@github.com:qt/qtbase.git
-        GIT_TAG         v6.9.1
-        GIT_SHALLOW     TRUE          # Enables --depth 1
-        GIT_PROGRESS    TRUE
-)
-FetchContent_MakeAvailable(QtBase)
+# FetchContent_Declare(QtBase
+#         GIT_REPOSITORY  git@github.com:qt/qtbase.git
+#         GIT_TAG         v6.9.1
+#         GIT_SHALLOW     TRUE          # Enables --depth 1
+#         GIT_PROGRESS    TRUE
+# )
+# FetchContent_MakeAvailable(QtBase)
 
