35484b69aa3f404d4414a58ce8d329a6c571c6f9
Valerii Koniushenko <Valerii.Koniushenko@gmail.com>
2025-07-09 00:01:37 +0200

    Refactored

--- cmake/FetchDependencies.cmake
+++ cmake/FetchDependencies.cmake
@@ -1,38 +1,55 @@
-include (FetchContent)
+include(FetchContent)
 
-set_property (DIRECTORY PROPERTY EP_BASE dependencies)
+set(FETCHCONTENT_BASE_DIR deps)
 
 FetchContent_Declare(Utils
-        GIT_REPOSITORY  git@github.com:ValeriiKoniushenko/Utils.git
-        GIT_TAG         origin/develop
-        GIT_SHALLOW     TRUE          # Enables --depth 1
-        GIT_PROGRESS    TRUE
+        GIT_REPOSITORY https://github.com/ValeriiKoniushenko/Utils.git
+        GIT_TAG origin/develop
 )
+
 FetchContent_MakeAvailable(Utils)
+FetchContent_GetProperties(Utils SOURCE_DIR Utils_SOURCES)
+
+list(APPEND CMAKE_MODULE_PATH "${Utils_SOURCES}/cmake/FetchDependencies.cmake")
+include(${Utils_SOURCES}/cmake/FetchDependencies.cmake)
 
 
 FetchContent_Declare(Glfw
-        GIT_REPOSITORY  git@github.com:glfw/glfw.git
-        GIT_TAG         3.4
-        GIT_SHALLOW     TRUE          # Enables --depth 1
-        GIT_PROGRESS    TRUE
+        GIT_REPOSITORY https://github.com/glfw/glfw.git
+        GIT_TAG 3.4
+        GIT_SHALLOW TRUE
+        GIT_PROGRESS TRUE
 )
+set(GLFW_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
+set(GLFW_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+set(GLFW_BUILD_DOCS OFF CACHE BOOL "" FORCE)
+set(GLFW_INSTALL OFF CACHE BOOL "" FORCE)
+
 FetchContent_MakeAvailable(Glfw)
 
 
-FetchContent_Declare(BetterEnums
-        GIT_REPOSITORY  git@github.com:aantron/better-enums.git
-        GIT_TAG         0.11.3
-        GIT_SHALLOW     TRUE          # Enables --depth 1
-        GIT_PROGRESS    TRUE
-)
-FetchContent_MakeAvailable(BetterEnums)
-
 
 FetchContent_Declare(SpdLog
-        GIT_REPOSITORY git@github.com:gabime/spdlog.git
+        GIT_REPOSITORY https://github.com/gabime/spdlog.git
         GIT_TAG v1.15.3
-        GIT_SHALLOW TRUE          # Enables --depth 1
+        GIT_SHALLOW TRUE
         GIT_PROGRESS TRUE
 )
+set(SPDLOG_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
+set(SPDLOG_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+set(SPDLOG_BUILD_BENCH OFF CACHE BOOL "" FORCE)
+
 FetchContent_MakeAvailable(SpdLog)
+
+
+# FetchContent_Declare(Assimp
+#         GIT_REPOSITORY https://github.com/assimp/assimp.git
+#         GIT_TAG v6.0.2
+#         GIT_SHALLOW TRUE
+#         GIT_PROGRESS TRUE
+# )
+# set(ASSIMP_BUILD_ZLIB ON)
+# set(ASSIMP_BUILD_ASSIMP_TOOLS ON)
+# set(ASSIMP_BUILD_TESTS OFF)
+#
+# FetchContent_MakeAvailable(Assimp)
