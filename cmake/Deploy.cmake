9fdcc90548dfb2dc5926531e9f1be5fb3ea53829
Valerii Koniushenko <Valerii.Koniushenko@gmail.com>
2025-07-21 14:53:10 +0200

    Fixed win32 behavior

--- cmake/Deploy.cmake
+++ cmake/Deploy.cmake
@@ -1,15 +1,18 @@
 
 if(WIN32)
-    add_custom_target(symlinks_to_assets ALL
-            COMMAND ${CMAKE_COMMAND} -E create_symlink --directory "${CMAKE_SOURCE_DIR}/assets/" "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/assets"
-            COMMAND ${CMAKE_COMMAND} -E create_symlink --directory "${CMAKE_SOURCE_DIR}/editor/" "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/editor"
-    )
+    file(TO_NATIVE_PATH "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/assets" _dstDir)
+    file(TO_NATIVE_PATH "${CMAKE_SOURCE_DIR}/assets/" _srcDir)
+    execute_process(COMMAND cmd.exe /c mklink /J "${_dstDir}" "${_srcDir}")
+
+    file(TO_NATIVE_PATH "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/editor" _dstDir)
+    file(TO_NATIVE_PATH "${CMAKE_SOURCE_DIR}/editor/" _srcDir)
+    execute_process(COMMAND cmd.exe /c mklink /J "${_dstDir}" "${_srcDir}")
 else()
     add_custom_target(symlinks_to_assets ALL
             COMMAND ${CMAKE_COMMAND} -E create_symlink "${CMAKE_SOURCE_DIR}/assets/" "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/assets"
             COMMAND ${CMAKE_COMMAND} -E create_symlink "${CMAKE_SOURCE_DIR}/editor/" "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/editor"
     )
+    add_dependencies(deploy symlinks_to_assets)
 endif()
 
 add_custom_target(deploy)
-add_dependencies(deploy symlinks_to_assets)
