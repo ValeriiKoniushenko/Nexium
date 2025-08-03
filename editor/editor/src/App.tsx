d13b14bd6b7b4cd1fe88c8001b8506ed4dd14bfe
Valerii Koniushenko <Valerii.Koniushenko@gmail.com>
2025-08-03 04:52:37 +0200

    New editor

--- editor/editor/src/App.tsx
+++ editor/editor/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import './App.css'
+import './App.scss'
 import Top from './components/Top'
 import Bottom from './components/Bottom'
 import RootLayout from './components/RootLayout'
