540d9f520d9660241b8f9e1fea4dd6c44faaf071
Valerii Koniushenko <Valerii.Koniushenko@gmail.com>
2025-08-20 00:14:58 +0200

    Fixed build issue

--- cmake/FetchDependencies.cmake
+++ cmake/FetchDependencies.cmake
@@ -78,7 +78,10 @@ set(ASSIMP_BUILD_ASSIMP_TOOLS ON)
 set(ASSIMP_BUILD_TESTS OFF)
 set(ASSIMP_BUILD_ASSIMP_TOOLS OFF)
 
+
 FetchContent_MakeAvailable(Assimp)
+target_compile_options(assimp PRIVATE /EHsc)
+target_compile_options(assimp PRIVATE /wd4530)
 
 
 FetchContent_Declare(Utils
