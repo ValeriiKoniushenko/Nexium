fcef631b0b27d6b8e818bcdff0fd1bb357ce30d7
Valerii Koniushenko <Valerii.Koniushenko@gmail.com>
2025-10-13 22:06:07 +0200

    Refactored NX editor architecture

--- sources/Editor/Windows/NxActorBasedEditor.h
+++ sources/Editor/Windows/NxActorBasedEditor.h
@@ -25,16 +25,11 @@
 #pragma once
 
 #include "AssetsManager/Mesh3DAsset.h"
-#include "BaseWindow.h"
-#include "Editor/GuiComponents/Array.h"
-#include "Editor/GuiComponents/Button.h"
 #include "Editor/GuiComponents/CheckBox.h"
-#include "Editor/GuiComponents/Combo.h"
 #include "Editor/GuiComponents/HorizontalLayout.h"
 #include "Editor/GuiComponents/Input.h"
 #include "Editor/GuiComponents/LabelRow.h"
 #include "Editor/GuiComponents/Misc.h"
-#include "Editor/GuiComponents/Separator.h"
 #include "Editor/GuiComponents/VecInput.h"
 #include "Editor/GuiComponents/VerticalLayout.h"
 #include "GameplaySystem/Framework/GameInstance.h"
