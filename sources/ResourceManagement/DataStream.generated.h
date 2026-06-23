5b4a2764fe0e6319009e71fe466d8a08ff3c813e
Valerii Koniushenko <Valerii.Koniushenko@gmail.com>
2026-06-23 23:55:39 +0200

    refactor(.gitattributes, .gitignore): update patterns for better version control
    - Added patterns for `.ini` and `.json` files in `data/config/` and `data/editor/` directories to use designated diff and merge utilities.
    - Expanded `.gitignore` to exclude additional directories and file types such as `/more/`, `string_metrics.csv`, `.jrm/cache.data`, `/cache`, and all `*.generated.*`.
    
    refactor(CMakeLists.txt): add conditional checks for ccache and MSVC compiler flags
    
    - Added conditional checks in CMakeLists.txt to search for and use ccache if available.
    - Modified MSVC debug and release-with-debuginfo flags by replacing "/Zi" with "/Z7".
    - Set `CMAKE_CXX_SCAN_FOR_MODULES` to OFF.
    
    chore(dependencies): update subproject commits
    
    - Updated subprojects in the `dependencies/` directory:
      - `JustReflectMe`: from `492e5dcc76ae065f1f1884233bd6d00d84095940` to `4f8c06876bdb6e31af9075bc1a4ffb4e0925cdf1`
      - `Utils`: from `1377ad7db905456bf2454fd9a96be74252d926c5` to `8f32b9877b4ef83517cb5f28a4b1115964e2680e`
      - `assimp`: from `e0b52347c6e52de2827ec957a9ebf00ce3c54f79` to `818f73f99ab429928d5b5c406352a356939a420b`
      - `freetype`: from `526ec5c47b9ebccc4754c85ac0c0cdf7c85a5e9b` to `25a08f24cfc0da879d1938352d026532f280b77e`
      - `glfw`: from `7b6aead9fb88b3623e3b3725ebb42670cbe4c579` to `567b1ec2442d59525e24c19e8d413df6baf02496`
      - `nlohmann_json`: from `55f9368` to `25c58ac`
      - `spdlog`: from `79524ddd08a4ec981b7fea76afd08ee05f83755d` to `f544fce006850a58063277068948dcda5681a27c`
    
    chore(sources): handle binary changes in generated headers
    
    - Updated binary files in the `sources/` directory:
      - `Widget.generated.h`
      - `GraphicsComponents.generated.h`
      - `Window.generated.h`
      - `DataStream.generated.h`
    
    These updates improve version control and ensure consistent handling of different file types, while also managing dependencies for various libraries and tools.

--- sources/ResourceManagement/DataStream.generated.h
+++ sources/ResourceManagement/DataStream.generated.h
@@ -13,77 +13,6 @@
 #include <unordered_map>
 
 template<>
-struct R<Core::DataStream::Mode>
-{
-    static constexpr std::string_view Name() { return "Mode"; }
-    static constexpr std::size_t Size() { return 2; }
-    static constexpr std::string_view ParentScope() { return "Core::DataStream"; }
-
-    static std::string_view ToString(::Core::DataStream::Mode value)
-    {
-        const auto& data = R<Core::DataStream::Mode>::ToMapCN();
-        const auto it = data.find(value);
-        if (it != data.end()) [[likely]]
-        {
-            return it->second;
-        }
-        static const std::string_view empty{};
-        return empty;
-    }
-
-    static std::optional<::Core::DataStream::Mode> FromString(std::string_view value)
-    {
-        const auto& data = R<Core::DataStream::Mode>::ToMapNC();
-        const auto it = data.find(value);
-        if (it != data.end()) [[likely]]
-        {
-            return it->second;
-        }
-        return std::nullopt;
-    }
-
-    static constexpr const std::array<::Core::DataStream::Mode, 2>& ToArrayC()
-    {
-        static constexpr std::array<::Core::DataStream::Mode, 2> constants = {
-				::Core::DataStream::Mode::Input,
-				::Core::DataStream::Mode::Output
-        };
-
-        return constants;
-    }
-
-    static constexpr const std::array<std::string_view, 2>& ToArrayN()
-    {
-        static constexpr std::array<std::string_view, 2> names = {
-				std::string_view("Input"),
-				std::string_view("Output")
-        };
-
-        return names;
-    }
-
-    static const std::unordered_map<::Core::DataStream::Mode, std::string_view>& ToMapCN()
-    {
-        static const std::unordered_map<::Core::DataStream::Mode, std::string_view> map = {
-				{ ::Core::DataStream::Mode::Input, "Input" },
-				{ ::Core::DataStream::Mode::Output, "Output" }
-        };
-
-        return map;
-    }
-
-    static const std::unordered_map<std::string_view, ::Core::DataStream::Mode>& ToMapNC()
-    {
-        static const std::unordered_map<std::string_view, ::Core::DataStream::Mode> map = {
-				{ "Input", ::Core::DataStream::Mode::Input },
-				{ "Output", ::Core::DataStream::Mode::Output }
-        };
-
-        return map;
-    }
-}; // struct R<Core::DataStream::Mode>
-
-template<>
 struct R<Core::DataStream::Result>
 {
     static constexpr std::string_view Name() { return "Result"; }
@@ -165,5 +94,76 @@ struct R<Core::DataStream::Result>
         return map;
     }
 }; // struct R<Core::DataStream::Result>
+
+template<>
+struct R<Core::DataStream::Mode>
+{
+    static constexpr std::string_view Name() { return "Mode"; }
+    static constexpr std::size_t Size() { return 2; }
+    static constexpr std::string_view ParentScope() { return "Core::DataStream"; }
+
+    static std::string_view ToString(::Core::DataStream::Mode value)
+    {
+        const auto& data = R<Core::DataStream::Mode>::ToMapCN();
+        const auto it = data.find(value);
+        if (it != data.end()) [[likely]]
+        {
+            return it->second;
+        }
+        static const std::string_view empty{};
+        return empty;
+    }
+
+    static std::optional<::Core::DataStream::Mode> FromString(std::string_view value)
+    {
+        const auto& data = R<Core::DataStream::Mode>::ToMapNC();
+        const auto it = data.find(value);
+        if (it != data.end()) [[likely]]
+        {
+            return it->second;
+        }
+        return std::nullopt;
+    }
+
+    static constexpr const std::array<::Core::DataStream::Mode, 2>& ToArrayC()
+    {
+        static constexpr std::array<::Core::DataStream::Mode, 2> constants = {
+				::Core::DataStream::Mode::Input,
+				::Core::DataStream::Mode::Output
+        };
+
+        return constants;
+    }
+
+    static constexpr const std::array<std::string_view, 2>& ToArrayN()
+    {
+        static constexpr std::array<std::string_view, 2> names = {
+				std::string_view("Input"),
+				std::string_view("Output")
+        };
+
+        return names;
+    }
+
+    static const std::unordered_map<::Core::DataStream::Mode, std::string_view>& ToMapCN()
+    {
+        static const std::unordered_map<::Core::DataStream::Mode, std::string_view> map = {
+				{ ::Core::DataStream::Mode::Input, "Input" },
+				{ ::Core::DataStream::Mode::Output, "Output" }
+        };
+
+        return map;
+    }
+
+    static const std::unordered_map<std::string_view, ::Core::DataStream::Mode>& ToMapNC()
+    {
+        static const std::unordered_map<std::string_view, ::Core::DataStream::Mode> map = {
+				{ "Input", ::Core::DataStream::Mode::Input },
+				{ "Output", ::Core::DataStream::Mode::Output }
+        };
+
+        return map;
+    }
+}; // struct R<Core::DataStream::Mode>
 
 // clang-format on
