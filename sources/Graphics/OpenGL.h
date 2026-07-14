c2405168a037e09a62c2e5a4f862355d33a864f7
Valerii Koniushenko <Valerii.Koniushenko@gmail.com>
2026-07-14 21:14:51 +0200

    Fixed Linux build issue

--- sources/Graphics/OpenGL.h
+++ sources/Graphics/OpenGL.h
@@ -28,9 +28,15 @@
 
 // clang-format off
 #include "glad/glad.h"
+#ifdef _WIN32
 #define GLFW_EXPOSE_NATIVE_WIN32
+#endif
+
 #include "GLFW/glfw3.h"
+
+#ifdef _WIN32
 #include "GLFW/glfw3native.h"
+#endif
 
 // clang-format on
 #include "JustReflectMe/Adapter.h"
