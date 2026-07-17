d5529b6adc016c07b7d7151a2c488c872d521aae
Valerii Koniushenko <Valerii.Koniushenko@gmail.com>
2026-07-17 02:39:02 +0200

    Modified .gitlab-ci.yml and testing it

--- sources/Editor/Gizmo.cpp
+++ sources/Editor/Gizmo.cpp
@@ -95,6 +95,8 @@ namespace Core
             const char directionChar
                 = static_cast<char>(toupper(touchedMesh->getComponentName()[0])) - 'X';
 
+            int* i = NULL;
+            
             data.direction = static_cast<Direction>(directionChar);
 
             for (auto& obj :
