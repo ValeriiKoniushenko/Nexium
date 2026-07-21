044d6c8e4e1d5ba954a4ea74cc453f6f7f999d06
Valerii Koniushenko <Valerii.Koniushenko@gmail.com>
2026-07-21 22:27:50 +0200

    NX-51: Gui::ListView - setRegexFilter doesn't work

--- sources/Editor/GuiComponents/List.cpp
+++ sources/Editor/GuiComponents/List.cpp
@@ -67,7 +67,7 @@ namespace Core::Gui
         {
             for (std::size_t i = 0; i < _items.size(); ++i)
             {
-                if (!_filter.isEmpty() && !_items.at(i).regexMatch(_filter))
+                if (!_filter.isEmpty() && !_items.at(i).regexFind(_filter))
                 {
                     continue;
                 }
