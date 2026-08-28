9e7a97b5793498fc0283e5410ee7c0dac9f612ed
Valerii Koniushenko <Valerii.Koniushenko@gmail.com>
2026-08-29 01:33:56 +0200

    refactor(tests): simplify FullSerializationCheck test by removing dynamic memory allocation
    - Removed dynamic memory allocation in FullSerializationCheck to simplify the test and mitigate potential memory management issues.

--- tests/ECSTests/ECSTests.cpp
+++ tests/ECSTests/ECSTests.cpp
@@ -944,8 +944,6 @@ namespace
 
 TEST_F(ECSTreeVehicleTests, FullSerializationCheck)
 {
-    int* i = new int(1);
-
     const auto serialized = R<decltype(root)>::Serialize(root).getData();
 
     // ROOT FIELDS
