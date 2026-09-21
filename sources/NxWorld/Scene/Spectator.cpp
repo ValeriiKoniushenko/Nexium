40ced849777d6e8961950036d170d4a11e0014b6
Valerii Koniushenko <Valerii.Koniushenko@gmail.com>
2026-09-21 19:53:05 +0200

    refactor(Scene): comment out keyboard movement input handling in Spectator.cpp for potential refactoring
    - The `getSpeed` lambda and `bindMovement` function were commented out to facilitate a review of the movement input logic and to possibly simplify the keyboard handling in the future.

--- sources/NxWorld/Scene/Spectator.cpp
+++ sources/NxWorld/Scene/Spectator.cpp
@@ -34,32 +34,35 @@ namespace NX
     {
         Actor::onInitialize();
 
-        const auto getSpeed = [this](Platform::Keyboard::KeyState leftShift)
-        { return speed / (leftShift == Platform::Keyboard::KeyState::Pressed ? 8.f : 1.f); };
-
-        const auto bindMovement = [this, &getSpeed](const char* name, Platform::Keyboard::Key key,
-                                                    auto movement, float direction)
-        {
-            _subscriptionPool << keyboardInput.getOrCreate(name, key)->onPress->subscribeAndGetID(
-                [=](KeyboardIA::SpecKeysState state)
-                {
-                    movement(direction * getSpeed(state.leftShift)
-                             * gGameInstance->world.getTimeDelta());
-                });
-        };
-
-        bindMovement(
-            "Move forward", Platform::Keyboard::Key::W, [this](float v) { moveForward(v); }, -1.f);
-        bindMovement(
-            "Move backward", Platform::Keyboard::Key::S, [this](float v) { moveForward(v); }, 1.f);
-        bindMovement(
-            "Move right", Platform::Keyboard::Key::D, [this](float v) { moveRight(v); }, 1.f);
-        bindMovement(
-            "Move left", Platform::Keyboard::Key::A, [this](float v) { moveRight(v); }, -1.f);
-        bindMovement("Move up", Platform::Keyboard::Key::R, [this](float v) { moveUp(v); }, 1.f);
-        bindMovement("Move down", Platform::Keyboard::Key::F, [this](float v) { moveUp(v); }, -1.f);
-
         // ==== 3D ====
+        // const auto getSpeed = [this](Platform::Keyboard::KeyState leftShift)
+        // { return speed / (leftShift == Platform::Keyboard::KeyState::Pressed ? 8.f : 1.f); };
+        // const auto bindMovement = [this, &getSpeed](const char* name, Platform::Keyboard::Key
+        // key,
+        //                                             auto movement, float direction)
+        // {
+        //     _subscriptionPool << keyboardInput.getOrCreate(name,
+        //     key)->onPress->subscribeAndGetID(
+        //         [=](KeyboardIA::SpecKeysState state)
+        //         {
+        //             movement(direction * getSpeed(state.leftShift)
+        //                      * gGameInstance->world.getTimeDelta());
+        //         });
+        // };
+        //
+        // bindMovement(
+        //     "Move forward", Platform::Keyboard::Key::W, [this](float v) { moveForward(v); },
+        //     -1.f);
+        // bindMovement(
+        //     "Move backward", Platform::Keyboard::Key::S, [this](float v) { moveForward(v);
+        //     }, 1.f);
+        // bindMovement(
+        //     "Move right", Platform::Keyboard::Key::D, [this](float v) { moveRight(v); }, 1.f);
+        // bindMovement(
+        //     "Move left", Platform::Keyboard::Key::A, [this](float v) { moveRight(v); }, -1.f);
+        // bindMovement("Move up", Platform::Keyboard::Key::R, [this](float v) { moveUp(v); }, 1.f);
+        // bindMovement("Move down", Platform::Keyboard::Key::F, [this](float v) { moveUp(v); },
+        // -1.f);
         // _subscriptionPool << Platform::GetWindow().onMouseWheel->subscribeAndGetID(
         //     [s = Core::WeakPtr(this)](glm::vec2 offset)
         //     {
