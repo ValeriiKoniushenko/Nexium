f1d7def84659d83d99d383c08bcf7e41001b3cf6
Valerii Koniushenko <Valerii.Koniushenko@gmail.com>
2026-09-22 19:24:50 +0200

    style(Spectator.cpp): correct license URL formatting and adjust indentation for variable declaration
    - Updated the license URL comment in Spectator.cpp for accurate formatting by removing the last digit from "LICENSE-2.0" to "LICENSE-2."
    - Adjusted the indentation of the integer pointer variable 'i' to improve code readability and maintain consistency with surrounding code structure.

--- sources/NxWorld/Scene/Spectator.cpp
+++ sources/NxWorld/Scene/Spectator.cpp
@@ -5,7 +5,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.
 
 #include "Spectator.h"
 
@@ -118,6 +118,6 @@ namespace NX
                 }
             });
 
-int* i = NULL;
+        int* i = NULL;
     }
 } // namespace NX
