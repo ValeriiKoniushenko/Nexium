d83b59570ee2939093d2d27cff0ab468b402689d
Valerii Koniushenko <Valerii.Koniushenko@gmail.com>
2026-09-22 20:00:24 +0200

    fix(Spectator.cpp): update Apache License comment and adjust indentation for consistency
    - Updated the Apache License comment to "LICENSE-2.0" for accuracy.
    - Adjusted the indentation of the integer pointer 'i' declaration for improved code consistency.

--- sources/NxWorld/Scene/Spectator.cpp
+++ sources/NxWorld/Scene/Spectator.cpp
@@ -5,7 +5,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.
+//     http://www.apache.org/licenses/LICENSE-2.0
 
 #include "Spectator.h"
 
@@ -118,6 +118,6 @@ namespace NX
                 }
             });
 
-int* i = nullptr;
+        int* i = nullptr;
     }
 } // namespace NX
